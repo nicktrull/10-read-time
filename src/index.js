66343a00b6a32c1968deb1db07a327a7a19bae2e
Nick Trull <nick.trull@gmail.com>
2016-03-16 20:11:26 -0500

    add nav items

--- src/index.js
+++ src/index.js
@@ -9,16 +9,27 @@
 //   time.classList.add(`read-time`);
 //   entry.appendChild(time);
 // }
+const bodyEl = document.querySelector(`body`);
 const entry = document.querySelectorAll(`.entry`);
+const nav = document.createElement(`nav`);
+bodyEl.appendChild(nav);
 
 for (let i = 0; i < entry.length; i++) {
+  // Setup and looking up elements for the current article
   const singleEntry = entry[i];
   const entryInfo = singleEntry.querySelector(`.entry__info`);
+  const title = singleEntry.querySelector(`.entry__title`);
+  const entryText = singleEntry.querySelector(`.entry__content`).innerText;
+
+  // Show read time to user
   const time = document.createElement(`li`);
   time.classList.add(`read-time`);
   entryInfo.appendChild(time);
-
-  const timer = entry[i];
-  const entryText = timer.querySelector(`.entry__content`).innerText;
   time.innerText = `${readTime(entryText, 200)} mins`;
+
+  // Add link for current entry to the nav
+  const link = document.createElement(`a`);
+  nav.appendChild(link);
+  link.setAttribute(`href`, `#${singleEntry.id}`);
+  link.innerText = title.innerText;
 }
